2b23544fc54c23a6e33d97fdb621c40c2b316e52
Jihed Mastouri <jihedmastouri.inbox@gmail.com>
2023-12-04 14:35:06 +0100

    optimizing day3 y23

--- y23/d03/b.ts
+++ y23/d03/b.ts
@@ -2,19 +2,20 @@ import fs from 'node:fs';
 import path from 'node:path';
 import rl from 'node:readline';
 
-const location = path.join(__dirname, 'test');
+const location = path.join(__dirname, 'input');
 const fileStream = fs.createReadStream(location);
 const lines = rl.createInterface({ input: fileStream });
 
 let idx = 0;
-const nums: number[][][] = [];
-let prevLine: string[] = [];
+let sum = 0;
+let prevLine: string = '';
+let prevNums: number[][] = [];
 
-const strLookup = (str: string[], i: number) => {
+const strLookup = (str: string, i: number) => {
 	let leftN = '';
 	let rightN = '';
 
-	const isNum = (str: string[]) => (n: number) => !isNaN(Number(str[n]));
+	const isNum = (str: string) => (n: number) => !isNaN(Number(str[n]));
 	const isNumPos = isNum(str);
 
 	if (isNumPos(i - 1)) {
@@ -34,8 +35,7 @@ const strLookup = (str: string[], i: number) => {
 	}
 
 	if (isNumPos(i)) {
-		const temp = str[i];
-		return [Number(leftN + temp + rightN)];
+		return [Number(leftN + str[i] + rightN)];
 	}
 
 	const res = [];
@@ -47,19 +47,18 @@ const strLookup = (str: string[], i: number) => {
 lines.on('line', (l) => {
 	if (!l) return;
 
-	nums.push([]);
-	const chars = l.split('');
+	let temp: number[][] = [];
 
-	for (let i = 0; i < chars.length; i++) {
-		const char = chars[i];
+	for (let i = 0; i < l.length; i++) {
+		const char = l[i];
 		if (char === '*') {
-			let temp = strLookup(chars, i);
+			let tp = strLookup(l, i);
 
 			if (prevLine.length !== 0) {
-				temp = [...temp, ...strLookup(prevLine, i)];
+				tp = [...tp, ...strLookup(prevLine, i)];
 			}
 
-			nums[idx].push(temp);
+			temp.push(tp);
 		}
 	}
 
@@ -67,29 +66,19 @@ lines.on('line', (l) => {
 	for (let i = 0; i < prevLine.length; i++) {
 		const char = prevLine[i];
 		if (char === '*') {
-			nums[idx - 1][j] = [...nums[idx - 1][j], ...strLookup(chars, i)];
+			prevNums[j] = [...prevNums[j], ...strLookup(l, i)];
 			j++;
 		}
 	}
 
-	prevLine = chars.slice();
+	sum += prevNums.reduce(
+		(acc, el) => (el.length !== 2 ? acc : acc + el[0] * el[1]),
+		0
+	);
+
+	prevLine = l;
+	prevNums = temp.map(el => el.slice());
 	idx++;
 });
 
-lines.on('close', () =>
-	console.log(
-		nums.reduce(
-			(acc, el) =>
-				acc +
-				el.reduce(
-					(acc, el) =>
-						el.flat().length !== 2
-							? acc
-							: acc + el.flat()[0] * el.flat()[1],
-					0
-				),
-
-			0
-		)
-	)
-);
+lines.on('close', () => console.log(sum));
